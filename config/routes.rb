97dd154f647766b3ed22ef81724fa532022a165b
Zhipeng <mrzpding@gmail.com>
2023-01-18 16:44:38 -0600

    Used custom routing, modified the movies#index/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'production_companies/index'
-  get 'production_companies/show'
-  get 'movies/index'
-  get 'movies/show'
-  get 'rails/g'
-  get 'rails/controller'
-  get 'rails/Movies'
-  get 'rails/index'
-  get 'rails/show'
+  get "production_companies/index"
+  get "production_companies/show"
+  get "movies/index"
+  get "movies/show"
+  get "rails/g"
+  get "rails/controller"
+  get "rails/Movies"
+  get "rails/index"
+  get "rails/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -18,4 +18,7 @@ Rails.application.routes.draw do
   #   GET /movies/:index
   #   GET /production_companies
   #   GET /production_companies/:id
+
+  resources :movies, only: %i[index show]
+  resources :production_companies, only: %i[index show]
 end
