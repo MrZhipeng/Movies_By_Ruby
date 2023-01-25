8de90fa17ed62be941924c40a04eb5b9713eb1e0
Zhipeng <mrzpding@gmail.com>
2023-01-25 16:34:02 -0600

    Search Feature Implemented

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   belongs_to :production_company
   has_many :movie_genres
-  has_many :genres, through: :MovieGenre
+  has_many :genres, through: :movie_genres
 end
